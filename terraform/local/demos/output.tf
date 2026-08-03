acabdb42f55460485fd83c372d998a98614d6c7e
Alvaro Mateo <alvaromateo9@gmail.com>
2026-08-03 21:56:26 +0200

    Fix ingress-configurator passthrough

--- terraform/local/demos/output.tf
+++ terraform/local/demos/output.tf
@@ -1,9 +1,29 @@
+output "vm_ip" {
+  description = "External (Multipass) IP address of the VM, reachable from the host."
+  value       = module.host_access.vm_ip
+}
+
+output "haproxy_ip" {
+  description = "Public address of the HAProxy unit inside the VM."
+  value       = module.host_access.haproxy_ip
+}
+
+output "ports" {
+  description = "TCP ports forwarded from the VM to HAProxy."
+  value       = module.host_access.ports
+}
+
+output "hostnames" {
+  description = "Hostnames served by the ingress."
+  value       = [for cfg in var.pr_apps : cfg.hostname]
+}
+
+output "ca_certificate" {
+  description = "PEM-encoded CA certificate from the self-signed-certificates charm."
+  value       = module.host_access.ca_certificate
+}
+
 output "gateway_ip" {
   description = "Discovered external IP of the gateway that HAProxy routes to."
   value       = module.demos_ps7.gateway_ip
-}
-
-output "pr_hostnames" {
-  description = "Map of PR app name to its routed hostname."
-  value       = { for name, cfg in var.pr_apps : name => cfg.hostname }
 }
